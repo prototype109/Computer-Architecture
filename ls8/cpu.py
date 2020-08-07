7cec38e7a2ad1a921836cff01d9139d8cc3285d0
Brandon Nichols <brandonaronnichols@gamil.com>
2020-08-07 15:55:26 -0500

    can dynamically get file

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,6 +1,7 @@
 """CPU functionality."""
 
 import sys
+import fileio
 
 HLT = 0b00000001
 LDI = 0b10000010
@@ -23,15 +24,7 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        program = fileio.command_list
 
         for instruction in program:
             self.ram[address] = instruction
