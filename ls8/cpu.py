43d4ebff8ff8ec5889c29714a4fb86d98d04a766
Brandon Nichols <brandonaronnichols@gamil.com>
2020-08-11 20:07:33 -0500

    completed implementation of stack and change elif block for checking commands to dispatchtable

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,16 +8,23 @@ LDI = 0b0010
 PRN = 0b0111
 ADD = 0b0000
 MUL = 0b0010
+PUSH = 0b0101
+POP = 0b0110
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
+        self.dispatchtable = {}
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
         self.ir = HLT
+        self.dispatchtable[LDI] = self.ldi
+        self.dispatchtable[PRN] = self.prn
+        self.dispatchtable[PUSH] = self.push
+        self.dispatchtable[POP] = self.pop
 
     def load(self):
         """Load a program into memory."""
@@ -69,8 +76,33 @@ class CPU:
 
         print()
 
+    def ldi(self):
+        register = self.ram_read(self.pc + 1)
+        value = self.ram_read(self.pc + 2)
+        self.reg[register] = value
+
+    def prn(self):
+        register = self.ram_read(self.pc + 1)
+        value = self.reg[register]
+        print(value)
+
+    def push(self):
+        self.reg[7] -= 1
+        current_sp = self.reg[7]
+        register = self.ram_read(self.pc + 1)
+        value = self.reg[register]
+        self.ram_write(value, current_sp)
+
+    def pop(self):
+        current_sp = self.reg[7]
+        value = self.ram_read(current_sp)
+        register = self.ram_read(self.pc + 1)
+        self.reg[register] = value
+        self.reg[7] += 1
+
     def run(self):
         """Run the CPU."""
+        self.reg[7] = 0xF4 
         self.ir = self.ram[self.pc]
 
         command = self.ir & 0b00001111
@@ -85,15 +117,7 @@ class CPU:
 
             if is_alu:
                 self.alu(command, self.ram_read(self.pc + 1), self.ram_read(self.pc + 2))
-            else:
-                if command == LDI:
-                    register = self.ram_read(self.pc + 1)
-                    value = self.ram_read(self.pc + 2)
-                    self.reg[register] = value
-
-                if command == PRN:
-                    register = self.ram_read(self.pc + 1)
-                    value = self.reg[register]
-                    print(value)
+            elif command != HLT:
+                self.dispatchtable[command]()             
                 
             self.pc += num_operands + 1
