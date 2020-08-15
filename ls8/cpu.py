d01440de4334bf18eac950a447a52ff2dd2ebdc4
Brandon Nichols <brandonaronnichols@gamil.com>
2020-08-14 20:58:26 -0500

    completed sprint

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,6 +12,10 @@ PUSH = 0b01000101
 POP = 0b01000110
 CALL = 0b01010000
 RET = 0b00010001
+CMP = 0b10100111
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
 
 class CPU:
     """Main CPU class."""
@@ -23,12 +27,16 @@ class CPU:
         self.reg = [0] * 8
         self.pc = 0
         self.ir = HLT
+        self.fl = 0
         self.dispatchtable[LDI] = self.ldi
         self.dispatchtable[PRN] = self.prn
         self.dispatchtable[PUSH] = self.push
         self.dispatchtable[POP] = self.pop
         self.dispatchtable[CALL] = self.call
         self.dispatchtable[RET] = self.ret
+        self.dispatchtable[JMP] = self.jmp
+        self.dispatchtable[JEQ] = self.jeq
+        self.dispatchtable[JNE] = self.jne
 
     def load(self):
         """Load a program into memory."""
@@ -57,6 +65,13 @@ class CPU:
         #elif op == "SUB": etc
         elif op == MUL:
             self.reg[reg_a] *= self.reg[reg_b]
+        elif op == CMP:
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.fl = 0b00000100
+            elif self.reg[reg_a] > self.reg[reg_b]:
+                self.fl = 0b00000010
+            else:
+                self.fl = 0b00000001
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -66,9 +81,9 @@ class CPU:
         from run() if you need help debugging.
         """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
+        print(f"TRACE: %02X | %02X %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
+            self.fl,
             #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
@@ -111,9 +126,7 @@ class CPU:
         self.ram_write(self.pc + 2, current_sp)
 
         #Point PC to subroutine
-        register = self.ram_read(self.pc + 1)
-        value = self.reg[register]
-        self.pc = value
+        self.jmp()
 
     def ret(self):
         current_sp = self.reg[7]
@@ -121,13 +134,29 @@ class CPU:
         self.pc = value
         self.reg[7] += 1
 
+    def jmp(self):
+        register = self.ram_read(self.pc + 1)
+        value = self.reg[register]
+        self.pc = value
+
+    def jeq(self):
+        if self.fl & 0b00000001:
+            self.jmp()
+        else:
+            self.pc += 2
+
+    def jne(self):
+        if not self.fl & 0b00000001:
+            self.jmp()
+        else:
+            self.pc += 2
+
     def run(self):
         """Run the CPU."""
         self.reg[7] = 0xF4 
         self.ir = self.ram[self.pc]
 
         command = self.ir
-        set_pc = self.ir >> 4 & 0b00000001
 
         while command != HLT:
 
